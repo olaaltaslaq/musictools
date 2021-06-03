74a6941181723da1d0f3a74c36b3000168d2f98a
OlaAlTaslaq <>
2021-06-03 13:45:46 +0000

    addjavas

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+document.write("<p style='text-align:center;'> WISH U ENJOYED AND THANK U FOR VISITING MY PAGE </p>")
+
+var favtool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL GUITAR OR PIANO ?");
+console.log(favtool);
+
+var x = "guitar";
+// var y = "piano";
+
+if (favtool = "x"){
+  alert("HI MUSIC LOVER")
+}
+
+// var img=document.createElement("img"); 
+// img.src="123.png";
+
+
