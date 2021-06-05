321e8aec5dc96c44c543eec7d2a97a7b08185ddd
OlaAlTaslaq <>
2021-06-05 15:27:05 +0000

    addpic

---
+++ script.js
@@ -0,0 +1,24 @@
+
+document.write("<p style='text-align:center;'> WISH U ENJOYED AND THANK U FOR VISITING MY PAGE </p>")
+
+document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://pics.me.me/when-you-meet-a-person-with-the-same-music-taste-38341656.png'>");
+
+document.write("<img  style='color:green; height: 400px;width: 33.4%;' src='https://data.whicdn.com/images/323764302/original.jpg'>");
+
+document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://i.pinimg.com/564x/61/08/15/610815ff632c6c8f68861ef99b111e5b.jpg'>");
+
+var userage = prompt("WHO OLD ARE YOU?");
+console.log(age);
+
+var age = 18;
+if (userage <= 18){
+  alert("HI MUSIC LOVER")
+}else{
+  alert("HI MUSIC FAN")
+}
+
+var favtool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL GUITAR OR PIANO?");
+console.log(tool);
+
+
+
