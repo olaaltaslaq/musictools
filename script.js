a60732900c98275dd9534e16a05db7e48fb7f14b
OlaAlTaslaq <>
2021-06-06 10:10:36 +0000

    addon

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ document.write('<p> Music is found in every known society, past and present, and
   document.write('<p> THANK U </p>')
 }
 
+var favtool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL GUITAR OR PIANO?");
+console.log(tool);
+
 document.write("<p style='text-align:center;'> WISH U ENJOYED AND THANK U FOR VISITING MY PAGE </p>")
 
 document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://pics.me.me/when-you-meet-a-person-with-the-same-music-taste-38341656.png'>");
@@ -33,16 +36,4 @@ document.write("<img  style='color:green; height: 400px;width: 33.4%;' src='http
 
 document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://i.pinimg.com/564x/61/08/15/610815ff632c6c8f68861ef99b111e5b.jpg'>");
 
-var userage = prompt("HOW OLD ARE YOU?");
-console.log(age);
-
-var age = 18;
-if (userage <= 18){
-  alert("HI MUSIC LOVER")
-}else{
-  alert("HELLO MUSIC FAN")
-}
-
-var favtool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL GUITAR OR PIANO?");
-console.log(tool);
 
