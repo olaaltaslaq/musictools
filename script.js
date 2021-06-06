2cd62871c0f15f99854ddbfc5c0372f6db134ba2
OlaAlTaslaq <>
2021-06-06 10:05:43 +0000

    putsome

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-
-var musicType = prompt(" +WHAT DO YOU LIKE A CLASIC MUSIC OR ROCK MUSIC?+ ");
-if (musicType == "clasic") {
-document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://abcmusic-production-au.s3-ap-southeast-2.amazonaws.com/s3fs-public/485%205247%20Classical%20Music%20to%20Lift%20Your%20Spirits.jpg'>");
-}else if (musicType == "rock"){
-document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://image.freepik.com/free-vector/rock-music-forever-poster-with-one-guitar-centre_1284-49237.jpg'>");
-}
-
-// document.write("<h3> " +userage+ " </h3>")
-// console.log(age);
 var userage = prompt("HOW OLD ARE YOU?");
+
 document.write("<h3> " +userage+ " </h3>")
 console.log(age);
 
@@ -19,6 +10,13 @@ if (userage <= 18){
   alert("HELLO MUSIC FAN")
 }
 
+
+var musicType = prompt(" +WHAT DO YOU LIKE A CLASIC MUSIC OR ROCK MUSIC?+ ");
+if (musicType == "clasic") {
+document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://abcmusic-production-au.s3-ap-southeast-2.amazonaws.com/s3fs-public/485%205247%20Classical%20Music%20to%20Lift%20Your%20Spirits.jpg'>");
+}else if (musicType == "rock"){
+document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://image.freepik.com/free-vector/rock-music-forever-poster-with-one-guitar-centre_1284-49237.jpg'>");
+}
 
 var musicHistory = prompt("WHAT DO YOU LIKE KOWN ABOUT MUISC HISTORY : yes or no ?");
 if (musicHistory == "yes" ){
