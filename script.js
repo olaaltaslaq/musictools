a36c2dc00cdcbbbd8f5311f2deffc65cf6ed1e5f
OlaAlTaslaq <>
2021-06-06 09:52:24 +0000

    addmorein

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 var musicType = prompt(" +WHAT DO YOU LIKE A CLASIC MUSIC OR ROCK MUSIC?+ ");
 if (musicType == "clasic") {
-
 document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://abcmusic-production-au.s3-ap-southeast-2.amazonaws.com/s3fs-public/485%205247%20Classical%20Music%20to%20Lift%20Your%20Spirits.jpg'>");
 }else if (musicType == "rock"){
 document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://image.freepik.com/free-vector/rock-music-forever-poster-with-one-guitar-centre_1284-49237.jpg'>");
 }
-
-
 
 document.write("<h3> " +userage+ " </h3>")
 console.log(age);
 
 var musicHistory = prompt("WHAT DO YOU LIKE KOWN ABOUT MUISC HISTORY : yes or no ?");
 if (musicHistory == "yes" ){
-document.write('<p> Though the term "classical music" includes all Western art music from the Medieval era to the 2000s, the Classical Era was the period of Western art music from the 1750s to the early 1820s—the era of Wolfgang Amadeus Mozart, Joseph Haydn, and Ludwig van Beethoven. </p>')
+document.write('<p> Music is found in every known society, past and present, and is considered to be a cultural universal. ... Consequently, the first music may have been invented in Africa and then evolved to become a fundamental constituent of human life, using various different materials to make various instruments. </p>')
 } else if (musicHistory == "no"){
   document.write('<p> THANK U </p>')
 }
@@ -40,7 +37,5 @@ if (userage <= 18){
 
 var favtool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL GUITAR OR PIANO?");
 console.log(tool);
-
-
 
 
