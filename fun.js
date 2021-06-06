1242dede3a4320c56192a42716a41f764b16665c
OlaAlTaslaq <>
2021-06-06 13:27:06 +0000

    addfunjs

--- fun.js
+++ fun.js
@@ -1,39 +1,45 @@
+var musictool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL: guiter OR piano ?");
 
-var musictool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL: GUITAR OR PIANO ?");
-
-function musictool() {
+function musictoolfun() {
 
   while (musictool != "guiter" && musictool != "piano") {
-    musictool = prompt("PLEASE CHOOSE YOUR FAVORITE MUSIC TOOL: GUITAR OR PIANO ?");
+
+    musictool = prompt("PLEASE CHOOSE YOUR FAVORITE MUSIC TOOL: guiter OR piano ?");
   }
 
-  document.write("<img scr='https://openai.com/content/images/2020/04/2x-no-mark-1.jpg'/>")
+  document.write("<img   style='color:green; height: 500px;width: 100%;'src='http://blog.siib.ac.in/wp-content/uploads/2013/07/siib-music.jpg'/>")
 
-  if (musictool == "guiter" || musictool == "piano") {
+  if (musictool == "guiter" ||  musictool == "piano") {
     console.log(musictool);
 
   } else {
-    musictool = prompt("PLEASE CHOOSE YOUR FAVORITE MUSIC TOOL: GUITAR OR PIANO ?");
+     musictool = prompt("please enter on of :  guiter, piano");
     console.log(musictool);
 
   }
+
 
 }
 
 musictoolfun();
 
 
-var x = prompt("HOW MANY TIMES DO YOU WANT TO SEE A MUSIC TOOLS IMAGE?")
-var toolsimgFun = function () {
 
-  var i = 0;
-  while (i < x) {
-    document.write("<img scr='https://i.ytimg.com/vi/x5S9RPVZyRw/maxresdefault.jpg'/>")
-    i++;
+var toolsimgfun = function ()
+ {
+  var x = prompt("HOW MANY TIMES DO YOU WANT TO SEE A MUSIC TOOLS IMAGE?")
+  var output= "" ;
+  for (var i=0 ; i < x ;i++) {
+    console.log(i)
+    console.log(i < x)
+    output=output+"<img  style='color:green; height: 500px;width: 100%;'src='https://i.ytimg.com/vi/x5S9RPVZyRw/maxresdefault.jpg'/>"
 
-    console.log(i, "----", x)
   }
+
+ document.write(output) 
 
 }
 
-toolsimgFun();
+
+toolsimgfun();
+
