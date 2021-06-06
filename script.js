4b6aa704e7ef683296bef1d2912cb90b2d49b309
OlaAlTaslaq <>
2021-06-06 11:04:20 +0000

    edit

--- script.js
+++ script.js
@@ -8,40 +8,28 @@ document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='http
 
 var userage = prompt("HOW OLD ARE YOU?");
 
-document.write("<h3> " +userage+ " </h3>")
+document.write("<h3> " + userage + " </h3>")
 console.log(age);
 
 var age = 18;
-if (userage <= 18){
+if (userage <= 18) {
   alert("HI MUSIC LOVER")
-}else{
+} else {
   alert("HELLO MUSIC FAN")
 }
 
 
 var musicType = prompt(" +WHAT DO YOU LIKE A CLASIC MUSIC OR ROCK MUSIC?+ ");
 if (musicType == "clasic") {
-document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://abcmusic-production-au.s3-ap-southeast-2.amazonaws.com/s3fs-public/485%205247%20Classical%20Music%20to%20Lift%20Your%20Spirits.jpg'>");
-}else if (musicType == "rock"){
-document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://image.freepik.com/free-vector/rock-music-forever-poster-with-one-guitar-centre_1284-49237.jpg'>");
+  document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://abcmusic-production-au.s3-ap-southeast-2.amazonaws.com/s3fs-public/485%205247%20Classical%20Music%20to%20Lift%20Your%20Spirits.jpg'>");
+} else if (musicType == "rock") {
+  document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://image.freepik.com/free-vector/rock-music-forever-poster-with-one-guitar-centre_1284-49237.jpg'>");
 }
 
 var musicHistory = prompt("WHAT DO YOU LIKE KOWN ABOUT MUISC HISTORY : yes or no ?");
-if (musicHistory == "yes" ){
-document.write('<p> Music is found in every known society, past and present, and is considered to be a cultural universal. ... Consequently, the first music may have been invented in Africa and then evolved to become a fundamental constituent of human life, using various different materials to make various instruments. </p>')
-} else if (musicHistory == "no"){
+if (musicHistory == "yes") {
+  document.write('<p> Music is found in every known society, past and present, and is considered to be a cultural universal. ... Consequently, the first music may have been invented in Africa and then evolved to become a fundamental constituent of human life, using various different materials to make various instruments. </p>')
+} else if (musicHistory == "no") {
   document.write('<p> THANK U </p>')
 }
-
-var favtool = prompt("WHAT IS YOUR FAVORITE MUSIC TOOL GUITAR OR PIANO?");
-console.log(tool);
-
-document.write("<p style='text-align:center;'> WISH U ENJOYED AND THANK U FOR VISITING MY PAGE </p>")
-
-document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://pics.me.me/when-you-meet-a-person-with-the-same-music-taste-38341656.png'>");
-
-document.write("<img  style='color:green; height: 400px;width: 33.4%;' src='https://data.whicdn.com/images/323764302/original.jpg'>");
-
-document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://i.pinimg.com/564x/61/08/15/610815ff632c6c8f68861ef99b111e5b.jpg'>");
-
 
