09040c0c73fe85d370d4ff0da8f4e3cbf183502c
OlaAlTaslaq <>
2021-06-06 10:12:32 +0000

    addinmore

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.write("<p style='text-align:center;'> WISH U ENJOYED AND THANK U FOR VISITING MY PAGE </p>")
+
+document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://pics.me.me/when-you-meet-a-person-with-the-same-music-taste-38341656.png'>");
+
+document.write("<img  style='color:green; height: 400px;width: 33.4%;' src='https://data.whicdn.com/images/323764302/original.jpg'>");
+
+document.write("<img  style='color:green; height: 400px;width: 33.3%;' src='https://i.pinimg.com/564x/61/08/15/610815ff632c6c8f68861ef99b111e5b.jpg'>");
+
 var userage = prompt("HOW OLD ARE YOU?");
 
 document.write("<h3> " +userage+ " </h3>")
